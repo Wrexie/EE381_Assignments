2f59dbc3df5d97b7d1304ec5d41683fcaf6a1172
drewbaetazar <60937655+drewbaetazar@users.noreply.github.com>
2021-02-05 18:07:41 -0800

    Lab 1 files

---
+++ Lab1/lab1_problem_1.py
@@ -0,0 +1,40 @@
+import numpy
+import random
+import matplotlib.pyplot as plt
+
+def nSidedDie(p):
+    cs = numpy.cumsum(p)
+    cp = numpy.append(0, cs)
+    n = len(p)
+    d = 0
+    r = random.random()
+    for j in range(n):
+        if r > cp[j] and r <= cp[j + 1]:
+            d = j + 1
+
+    return d
+
+def main():
+    p = [0.10, 0.15, 0.20, 0.05, 0.30, 0.10, 0.10]
+    N = 1000
+    s = numpy.zeros((N, 1))
+    for i in range(N):
+        r = nSidedDie(p)
+        s[i] = r
+
+    b = range(1, len(p) + 2)
+    sb = numpy.size(b)
+    h1, bin_edges = numpy.histogram(s, bins=b)
+    b1 = bin_edges[0:sb - 1]
+    plt.close('all')
+    prob = h1 / N
+    plt.stem(b1, prob)
+    # Graph labels
+    plt.title('PMF for an unfair ' + str(len(p)) + '-sided die')
+    plt.xlabel('Number on the face of the die')
+    plt.ylabel('Probability')
+    plt.xticks(b1)
+    plt.show()
+
+
+main()
