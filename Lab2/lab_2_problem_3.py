56050111d56fcff18d2ba1b0db54355313b5068e
Wrexie <61041168+Wrexie@users.noreply.github.com>
2021-02-25 21:00:50 -0800

    finished problem 3

---
+++ Lab2/lab_2_problem_3.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+
+def createMessageBit(bitProbability):
+    cs = np.cumsum(bitProbability)
+    cp = np.append(0, cs)
+    n = len(bitProbability)
+    m = np.random.rand()
+
+    for i in range(n):
+        if cp[i] < m <= cp[i + 1]:
+            bit = i
+
+    return bit
+
+
+def main():
+    p0 = [.6, .4]
+    e0 = [.95, .05]
+    e1 = [.03, .97]
+    N = 100000
+    successCount = 0
+    rOneCount = 0
+
+    for i in range(N):
+        S = createMessageBit(p0)
+        if S == 1:
+            R = createMessageBit(e1)
+        elif S == 0:
+            R = createMessageBit(e0)
+
+        if R == 1:
+            rOneCount += 1
+            if S == R:
+                successCount += 1
+
+    print("Probability that message will be received incorrectly is:", successCount / rOneCount)
+
+
+main()
