56458088b2e1a62ea3f065241b10ffefa56c9b47
drewbaetazar <baltazarcode@gmail.com>
2021-04-09 16:21:08 -0700

    worked on problem 3

---
+++ Lab4/lab_4_problem_3.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main():
+    B = 40
+    N = 10000
+
+
+    for i in range(N):
+        carton = np.ones(24)
+
+
+    return 0
+
+
+main()
+
