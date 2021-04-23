aa7df78e9a74ae655b140a58a80a46c42722d279
drewbaetazar <baltazarcode@gmail.com>
2021-04-23 16:36:48 -0700

    im confused af

--- Lab5/lab_5_problem_2.py
+++ Lab5/lab_5_problem_2.py
@@ -8,30 +8,37 @@ def main():
     sigma = 5
     n = 5
     sampleSizes = [5, 40, 120]
-    tValues = []
     populationDist = np.random.normal(mu, sigma, N)
     M = 10000
     normalDistributionSuccess = 0
     tDistributionSuccess = 0
+    confidenceIntervalType = [95, 99]
 
     for n in sampleSizes:
+        normalDistributionSuccess = 0
+        if(n == 5):
+            for interval in confidenceIntervalType:
+                if interval == 95:
+                    for i in range(M):
+                        sample = np.random.choice(populationDist, n)
+                        sampleMean = sample.mean()
+                        std = sigma / math.sqrt(n)
+                        upperConfidenceNormal = mu + 1.96 * std
+                        lowerConfidenceNormal = mu - 1.96 * std
 
-        for i in range(M):
-            sample = np.random.choice(populationDist, n)
-            sampleMean = sample.mean()
-            std = sigma / math.sqrt(n)
-            upperConfidenceNormal = mu + 1.96 * std
-            lowerConfidenceNormal = mu - 1.96 * std
+                        upperConfidenceT = mu + 2.78 * std
+                        lowerCondidenceT = mu - 2.78 * std
 
-            upperConfidenceT = mu + 2.78 * std
-            lowerCondidenceT = mu - 2.78 * std
+                        if upperConfidenceNormal >= sampleMean >= lowerConfidenceNormal:
+                            normalDistributionSuccess += 1
+                        if upperConfidenceT >= sampleMean >= lowerCondidenceT:
+                            tDistributionSuccess += 1
+            print("95% Confidence Interval test:")
+            print("    Number of successes of normal distribution of sample size %d: %d" % (n, normalDistributionSuccess))
+            print("    Success percentage of normal distribution: %d" % ((normalDistributionSuccess/M) * 100))
+            print("    Number of successes of t distribution of sample size %d: %d" % (n, tDistributionSuccess))
+            print("    Success percentage of t distribution: %d" % ((tDistributionSuccess/M) * 100))
 
-            if upperConfidenceNormal >= sampleMean >= lowerConfidenceNormal:
-                normalDistributionSuccess += 1
-
-
-
-    print(M/normalDistributionSuccess)
     return 0
 
 
